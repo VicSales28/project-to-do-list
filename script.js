9c3eae196687c155644d83341f6fbdcfac26f9d3
Victoria Sales <contatovictoriasales@gmail.com>
2022-11-14 22:21:55 -0300

    C5-Adiciona função clearAllTasks

--- script.js
+++ script.js
@@ -2,17 +2,25 @@ const newTaskInput = document.getElementById('texto-tarefa');
 const addTaskBtn = document.getElementById('criar-tarefa');
 const tasksList = document.getElementById('lista-tarefas');
 
-const removeSelectedBtn = document.getElementById('remover-selecionado');
-const clearCompletedBtn = document.getElementById('remover-finalizados');
-const clearAllBtn = document.getElementById('apaga-tudo');
+//const removeSelectedBtn = document.getElementById('remover-selecionado');
+//const clearCompletedBtn = document.getElementById('remover-finalizados');
+const clearAllTasksBtn = document.getElementById('apaga-tudo');
 
-const saveItemsBtn = document.getElementById('salvar-tarefas');
-const moveUpBtn = document.getElementById('mover-cima');
-const moveDownBtn = document.getElementById('mover-baixo');
+//const saveItemsBtn = document.getElementById('salvar-tarefas');
+//const moveUpBtn = document.getElementById('mover-cima');
+//const moveDownBtn = document.getElementById('mover-baixo');
 
 const clearTaskInput = () => {
   newTaskInput.value = '';
 };
+
+function changeItemBackgroundColor(event) {
+  const listItem = event.target;
+  if (document.querySelector('.selected')) {
+    document.querySelector('.selected').classList.remove('selected');
+  }
+  listItem.classList.add('selected');
+}
 
 const addNewTask = () => {
   const newListItem = document.createElement('li');
@@ -24,10 +32,10 @@ const addNewTask = () => {
 
 addTaskBtn.addEventListener('click', addNewTask);
 
-function changeItemBackgroundColor(event) {
-  const listItem = event.target;
-  if (document.querySelector('.selected')) {
-    document.querySelector('.selected').classList.remove('selected');
+function clearAllTasks() {
+  for (let index = tasksList.childNodes.length - 1; index >= 0; index -= 1) {
+    tasksList.removeChild(tasksList.childNodes[index]);
   }
-  listItem.classList.add('selected');
 }
+
+clearAllTasksBtn.addEventListener('click', clearAllTasks);
